2572f9eb7d3647b6864e3a80e5371f1719b1b4b8
opsaaaaa <sean@ferney.org>
2020-05-10 13:31:29 -0600

    making rotate code nicer

--- agents/player/Player.gd
+++ agents/player/Player.gd
@@ -13,7 +13,7 @@ export var player_number = 1
 
 var velocity = Vector2()
 var charge = 0.0
-var rot_speed = 7
+var rot_speed = 5
 var speed = 10
 
 onready var sprite = $Sprite
@@ -29,24 +29,25 @@ func _ready():
 
 
 func _launch():
-	
 	if Input.is_action_just_released(inputs[player_number]):
 		velocity = Vector2(0,speed).rotated(sprite.rotation) * charge
 		charge = 0
 
-
 func _charge_up(delta):
-	
 	if Input.is_action_pressed(inputs[player_number]):
 		charge += charge_speed
-		sprite.rotate(rot_speed * delta)
-	else:
-		sprite.rotate(-rot_speed * delta)
 
+func input_rotation():
+	return ((int(button_pressed())*2)-1) * rot_speed
+
+func button_pressed()->bool:
+	return Input.is_action_pressed(inputs[player_number])
 
 func _physics_process(delta):
 	
 	_charge_up(delta)
 	_launch()
 	velocity = velocity * FRICTION
+	print(input_rotation())
+	sprite.rotate(input_rotation() * delta)
 	move_and_slide(velocity * delta)
