2393f96668555ad0bf9e7895d3398a8fa1b3345d
opsaaaaa <sean@ferney.org>
2020-05-10 14:19:06 -0600

    moving functions around

--- agents/player/Player.gd
+++ agents/player/Player.gd
@@ -15,7 +15,7 @@ export(POLARITIES) var polaraty = POLARITIES.POSITIVE
 export var player_number = 1
 
 var velocity = Vector2()
-var charge = 1
+var charge = 0
 var rot_speed = 2
 var speed = MIN_SPEED
 
@@ -30,34 +30,44 @@ func _ready():
 		POLARITIES.NEGITIVE:
 			sprite.texture = negative_texture
 
+
 func _physics_process(delta):
-	_movement()
 	_launch()
 	_charge_up(delta)
-
 	
-	sprite.rotate(input_rotation() * delta)
-	move_and_slide(velocity * delta)
+	sprite.rotate( input_rotation() * delta )
+	move_and_slide( movement() * delta )
+
+
 
 
 func _launch():
-	if Input.is_action_just_released(inputs[player_number]):
+	if button_just_released():
 		speed = speed * charge
 		charge = 0
 
 func _charge_up(delta):
-	if Input.is_action_pressed(inputs[player_number]):
+	if button_pressed():
 		charge = min( charge + (charge_speed * delta), MAX_CHARGE )
+
+
 
 func input_rotation():
 	return ( ( int(button_pressed()) *2 ) - 1 ) * rot_speed
-
+	
+func movement()->Vector2:
+	speed = max( MIN_SPEED, speed * FRICTION)
+	return Vector2( 0 , speed ).rotated(sprite.rotation)
+	
+	
+	
 func button_pressed()->bool:
 	return Input.is_action_pressed(inputs[player_number])
+	
+func button_just_released()->bool:
+	return Input.is_action_just_released(inputs[player_number])
 
-func _movement()->void:
-	speed = max( MIN_SPEED, speed * FRICTION)
-	velocity = Vector2( 0 , speed ).rotated(sprite.rotation)
+
 
 
 
