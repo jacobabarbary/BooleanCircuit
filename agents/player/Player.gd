a4877d2fe9ecc8f4dcecc96f5253323652944c44
opsaaaaa <sean@ferney.org>
2020-05-10 14:11:58 -0600

    added max and minimum to charge and speed

--- agents/player/Player.gd
+++ agents/player/Player.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
 enum POLARITIES { POSITIVE, NEGITIVE }
-const charge_speed = 20
+
+const MAX_CHARGE = 10
+const MIN_SPEED = 6000
+const charge_speed = 2
 const FRICTION = 0.99
 
 var inputs = ["PlayerAction","Player2Action"]
@@ -12,9 +15,9 @@ export(POLARITIES) var polaraty = POLARITIES.POSITIVE
 export var player_number = 1
 
 var velocity = Vector2()
-var charge = 0.0
-var rot_speed = 5
-var speed = 10
+var charge = 1
+var rot_speed = 2
+var speed = MIN_SPEED
 
 onready var sprite = $Sprite
 onready var connector = $Connector
@@ -27,27 +30,34 @@ func _ready():
 		POLARITIES.NEGITIVE:
 			sprite.texture = negative_texture
 
+func _physics_process(delta):
+	_movement()
+	_launch()
+	_charge_up(delta)
+
+	
+	sprite.rotate(input_rotation() * delta)
+	move_and_slide(velocity * delta)
+
 
 func _launch():
 	if Input.is_action_just_released(inputs[player_number]):
-		velocity = Vector2(0,speed).rotated(sprite.rotation) * charge
+		speed = speed * charge
 		charge = 0
 
 func _charge_up(delta):
 	if Input.is_action_pressed(inputs[player_number]):
-		charge += charge_speed
+		charge = min( charge + (charge_speed * delta), MAX_CHARGE )
 
 func input_rotation():
-	return ((int(button_pressed())*2)-1) * rot_speed
+	return ( ( int(button_pressed()) *2 ) - 1 ) * rot_speed
 
 func button_pressed()->bool:
 	return Input.is_action_pressed(inputs[player_number])
 
-func _physics_process(delta):
-	
-	_charge_up(delta)
-	_launch()
-	velocity = velocity * FRICTION
-	print(input_rotation())
-	sprite.rotate(input_rotation() * delta)
-	move_and_slide(velocity * delta)
+func _movement()->void:
+	speed = max( MIN_SPEED, speed * FRICTION)
+	velocity = Vector2( 0 , speed ).rotated(sprite.rotation)
+
+
+
