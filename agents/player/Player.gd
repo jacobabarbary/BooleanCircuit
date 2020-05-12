58c0ad0b75e3536780d4789c1db6c54e35e1a1a4
jacob ferney <jacob@ferney.org>
2020-05-12 14:33:56 -0600

    redid movement

--- agents/player/Player.gd
+++ agents/player/Player.gd
@@ -1,13 +1,13 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 enum POLARITIES { POSITIVE, NEGITIVE }
 
-const MAX_CHARGE = 10
-const MIN_SPEED = 6000
-const charge_speed = 2
-const FRICTION = 0.99
+onready var sprite = $Sprite
+onready var connector = $Connector
 
+var inputs_pressed = []
 var inputs = ["PlayerAction","Player2Action"]
+var ray_connected
 
 export(int, 
 	"#", 'o', '-', 'yen' , '^', 'x' , '+', 'yang', 'sad', 'happy', 'eyes', 'skull'
@@ -17,53 +17,32 @@ export(Color) var color = Color.white
 	
 export(int, "PlayerAction","Player2Action") var player_number = 1
 
-var velocity = Vector2()
-var charge = 0
-var rot_speed = 2
-var speed = MIN_SPEED
-
-onready var sprite = $Sprite
-onready var connector = $Connector
+var rot_speed = 0.1
+var speed = 250
 
 func _ready():
 	set_skin(skin)
 	set_color(color)
 
+func _input(event):
+	
+	inputs_pressed = [
+		Input.is_action_pressed(inputs[player_number]),
+		Input.is_action_just_released(inputs[player_number]),
+		Input.is_action_just_pressed(inputs[player_number])]
+
 func _physics_process(delta):
-	_launch()
-	_charge_up(delta)
+	aim()
+	launch()
 	
-	sprite.rotate( input_rotation() * delta )
-	move_and_slide( movement() * delta )
 
+func aim():
+	if inputs_pressed[0]:
+		sprite.rotate(rot_speed)
 
-func _launch():
-	if button_just_released():
-		speed = speed * charge
-		charge = 0
-
-func _charge_up(delta):
-	if button_pressed():
-		charge = min( charge + (charge_speed * delta), MAX_CHARGE )
-
-
-
-func input_rotation():
-	return ( ( int(button_pressed()) *2 ) - 1 ) * rot_speed
-	
-func movement()->Vector2:
-	speed = max( MIN_SPEED, speed * FRICTION)
-	return Vector2( 0 , speed ).rotated(sprite.rotation)
-	
-	
-	
-func button_pressed()->bool:
-	return Input.is_action_pressed(inputs[player_number])
-	
-func button_just_released()->bool:
-	return Input.is_action_just_released(inputs[player_number])
-
-
+func launch():
+	if inputs_pressed[1]:
+		linear_velocity = Vector2(0,1).rotated(sprite.rotation) * speed
 
 func set_color(col: Color)->void:
 	color = col
@@ -73,4 +52,5 @@ func set_skin(index)->void:
 	skin = index
 	$Sprite.set_frame(index)
 
-
+func _on_ray_connected(is_ray_connected)->void:
+	ray_connected = is_ray_connected
