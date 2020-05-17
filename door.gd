2181f18b38c22fad17e6ef74463a12bceb2ab198
opsaaaaa <sean@ferney.org>
2020-05-16 20:33:57 -0600

    added color to the door

--- door.gd
+++ door.gd
@@ -1,5 +1,9 @@
 extends StaticBody2D
 
+
+const DISABLED_COLOR = Color(0.5,0.5,0.5,0.3)
+const ENABLED_COLOR = Color(1,0.5,0.5,1)
+
 export var door_type = 1
 
 
@@ -12,8 +16,9 @@ func on_triggered(type,triggered):
 	
 	if type == door_type and triggered == true:
 		collider.disabled = true
-		sprite.visible = false
+		sprite.modulate = DISABLED_COLOR
 	else:
 		collider.disabled = false
+		sprite.modulate = ENABLED_COLOR
 		sprite.visible = true
 
