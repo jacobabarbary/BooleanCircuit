b9467414b542aa487b9a4175c3fb286178b86fff
opsaaaaa <sean@ferney.org>
2020-05-16 20:02:59 -0600

    added pause menu

--- door.gd
+++ door.gd
@@ -5,12 +5,4 @@ onready var sprite = $Sprite
 onready var scene_root = get_tree().current_scene
 var type = 1
 
-func _process(delta):
-	
-	if typeof(scene_root.types[type]) == TYPE_BOOL:
-		if scene_root.types[type] == true:
-			collider.disabled = true
-			sprite.visible = false
-	else:
-		collider.disabled = false
-		sprite.visible = true
+
--- gui/game.gd
+++ gui/game.gd
@@ -1,5 +1,13 @@
 extends Control
 
+onready var pause_popup = $Pause
+onready var death_popup = $Death
 
-func _input(event):
-	pass
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_pause"):
+		pause_popup.visible = !pause_popup.visible
+
+func _on_all_players_disconnected():
+	death_popup.visible = true
